51978f59089b1529cbbd974f3582a684bf5348df
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-05 23:12:02 +0100

    Update main.js

--- map/main.js
+++ map/main.js
@@ -1,1 +1,117 @@
 /* An Interactive Map */
+
+// Setting up the map //
+const firstMap = L.map('map', {
+    center: [32.43, -6.06],
+    zoom: 3.75
+});
+
+const OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(firstMap);
+
+// Creating a GeoJSON object to represent the regions of Morocco
+L.geoJSON(regions).addTo(firstMap);
+
+// Adding custom style to GeoJSON objects //
+colors = ['#f7fcfd','#e0ecf4','#bfd3e6','#9ebcda','#8c96c6','#8c6bb1','#88419d','#810f7c','#4d004b']
+
+// Color gradient
+function getColor(c) {
+  return c > 8 ? '#810F7C':
+         c > 7.5 ? '#88419D':
+         c > 7  ? '#8C6BB1':
+         c > 6.5  ? '#8C96C6':
+         c > 5.5  ? '#9ebcda':
+         c > 3  ? '#bfd3e6':
+         c > 1.5   ? '#e0ecf4':
+         c > 1   ? '#F7FCFD':
+                    '#FFFFFF';
+};
+
+function style(feature) {
+  return ({
+    fillColor: getColor(feature.properties.population),
+    fillOpacity: 0.3,
+    color: '#FFF',
+    dashArray: '3',
+    weight: 2
+  })
+}
+
+let geojson = L.geoJSON(regions, {style: style}).addTo(firstMap);
+
+// Adding Some Interactions //
+
+// Highlighting Features
+function highlightFeatures(e) {
+  let layer = e.target;
+  layer.setStyle({
+    color: '#555',
+    dashArray: '',
+    fillOpacity: 1,
+    weight: 3
+  })
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+
+  controlPanel.update(layer.feature.properties);
+}
+
+// Reset the style as default
+function reset(e) {
+  geojson.resetStyle(e.target);
+  controlPanel.update();
+}
+
+// Zooming to the feature
+function zoomToFeature(e) {
+  firstMap.fitBounds(e.target.getBounds());
+}
+
+// Executing events listeners //
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeatures,
+    mouseout: reset,
+    click: zoomToFeature
+  })
+}
+
+// Adding custom control to the map //
+let controlPanel = L.control({position: 'topright'});
+controlPanel.onAdd = function() {
+  this.cPanel = L.DomUtil.create('div', 'control-panel');
+  this.update();
+  return this.cPanel;
+}
+
+controlPanel.update = function(data) {
+  this.cPanel.innerHTML = '<h4> Morocco Population Density </h4>' +  (data ?
+    '<div class = "name">' + data.name + '</div><br/>' + '<br/><div class = "output" >' + data.population + ' million people </div>'
+    : 'Hover over a region');
+}
+
+controlPanel.addTo(firstMap);
+
+// Adding a custom legend to the map //
+let legend = L.control({position: 'bottomleft'});
+legend.onAdd = function() {
+  let info = L.DomUtil.create('div', 'info');
+  grades = [0, 0.5, 1, 1.5, 3, 5.5, 6.5, 7, 7.5, 8];
+  for(let grade = 0; grade < grades.length; grade++) {
+
+    info.innerHTML += 
+    '<i style="background:' + getColor(grades[grade] + 1) + '"></i> ' +
+    grades[grade] + (grades[grade + 1] ? ' &ndash; ' + grades[grade + 1] + '<br>' : ' +');
+  }
+
+  return info;
+};
+
+legend.addTo(firstMap);
+
+L.geoJSON(regions, {style: style, onEachFeature: onEachFeature}).addTo(firstMap);
